a955a35bd8c0f1112488c184b01b1958718e212c
Brent Martin <bmart35@gmail.com>
2016-08-12 01:53:45 -0500

    wrapped each section up into methods

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,8 +1,20 @@
+def jason
+  start_day
+  work
+  finish
+  drink
+  end_day
+end
+
+def start_day
   @meetings = []
   @reviews = []
   @bugs = []
   @codes = []
   @deploy = []
+end
+
+def work
   rand(3..5).times do
     scotch = 1
     @meetings << scotch
@@ -29,12 +41,18 @@
     scotch = rand(-4..4)
     @deploy << scotch
   end
+end
+
+def finish
   @m_drinks = @meetings.inject(0) {|sum, i|  sum + i }
   @r_drinks = @reviews.inject(0) {|sum, i|  sum + i }
   @b_drinks = @bugs.inject(0) {|sum, i|  sum + i }
   @c_drinks = @codes.inject(0) {|sum, i|  sum + i }
   @d_drinks = @deploy.inject(0) {|sum, i|  sum + i }
   @total_drinks = @m_drinks + @r_drinks + @b_drinks + @c_drinks + @d_drinks
+end
+
+def drink
   system('clear')
   print "Jason had #{@meetings.count} meetings today. "
   print "He had #{@reviews.count} code reviews, "
@@ -45,6 +63,7 @@
   sleep 1
   puts "Lets guess how many drinks Jason will be having!?"
   guess
+end
 
 
 def guess
@@ -61,5 +80,8 @@ def guess
   else
     sleep 0.5
     puts "Nailed it! Good job."
+    sleep 3
   end
 end
+
+jason
