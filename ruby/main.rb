2f2e819901cf4527549a394abc0b0ffc1bd7a9d6
Brent Martin <bmart35@gmail.com>
2016-08-12 00:26:27 -0500

    used a times-loop paired and rand to iterate through the array and push in values for scotch

--- ruby/main.rb
+++ ruby/main.rb
@@ -3,3 +3,22 @@
   @bugs = []
   @codes = []
   @deploy = []
+  rand(3..5).times do
+    scotch = 1
+    @meetings << scotch
+  end
+
+  rand(1..5).times do
+    scotch = -1
+    @reviews << scotch
+  end
+
+  rand(0..4).times do
+    scotch = rand(0..2)
+    @bugs << scotch
+  end
+
+  rand(0..6).times do
+    scotch = rand(-1..1)
+    @codes << scotch
+  end
